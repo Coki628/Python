154ce6a468b566503a7a4f8623b908c563cc3f9e
MYCOMPUTER\srcn4 <srcn4frdm.c@gmail.com>
2018-05-16 00:57:42 +0900

    微調整

--- AtCoder/ABC097c2.py
+++ AtCoder/ABC097c2.py
@@ -7,19 +7,15 @@ target = int(input())
 str_list = []
 for i in range(len(instr)):
 
-	# 開始位置はi以降(i > jはやる意味ない)
-	for j in range(i, len(instr)+1):
+	# 開始位置はi+1以降(i > jはやる意味ない)
+	for j in range(i+1, len(instr)+1):
 		
 		# targetよりも大きい文字数のものは処理しない
 		if j-i > target:
 			break
 
 		# 候補となる部分文字列の格納
-		if i < j:
-			str_list.append(instr[i:j])
-
-		j += 1
-	i += 1
+		str_list.append(instr[i:j])
 
 # 重複削除
 str_list = list(set(str_list))
