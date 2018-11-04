548a5ec825cb04c85a4721b5ec76bd16234a29e1
MYCOMPUTER\srcn4 <srcn4frdm.c@gmail.com>
2018-11-05 01:52:53 +0800

    ちょい整理。

--- AtCoder/ABC113b.py
+++ AtCoder/ABC113b.py
@@ -4,12 +4,11 @@ N = int(input())
 T,A = map(int, input().split())
 hN = list(map(int, input().split()))
 
-tN = [0] * N
 ans = -1
 tmp = float('inf')
 for i in range(N):
-    tN[i] = T - hN[i] * 0.006
-    if abs(tN[i] - A) < tmp:
-        tmp = abs(tN[i] - A)
+    t = T - hN[i] * 0.006
+    if abs(t - A) < tmp:
+        tmp = abs(t - A)
         ans = i + 1
 print(ans)
