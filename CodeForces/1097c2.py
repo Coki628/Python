464fd8e959f3bf23cba916e77d53980d2ba7f2a2
MYCOMPUTER\srcn4 <srcn4frdm.c@gmail.com>
2019-01-07 09:23:35 +0700

    ちょっと追加編集

--- CodeForces/1097c2.py
+++ CodeForces/1097c2.py
@@ -11,27 +11,26 @@ def input(): return sys.stdin.readline().strip()
 from collections import deque, Counter
 
 N = int(input())
-aN = [input() for i in range(N)]
 
 cnt = {'l': Counter(), 'r': Counter(), 'lr': 0}
-for i in range(N):
-    bracket = Counter()
+for _ in range(N):
+    l = r = 0
     # 各行についてペア候補になる括弧を集計
-    for j in range(len(aN[i])):
-        if aN[i][j] == '(':
-            bracket['l'] += 1
+    for c in input():
+        if c == '(':
+            l += 1
         else:
-            if bracket['l'] != 0:
-                bracket['l'] -= 1
+            if l != 0:
+                l -= 1
             else:
-                bracket['r'] += 1
+                r += 1
     # どの状態の行がいくつあるか集計
-    if bracket['l'] == 0 and bracket['r'] == 0:
+    if l == 0 and r == 0:
         cnt['lr'] += 1
-    elif bracket['l'] == 0:
-        cnt['r'][bracket['r']] += 1
-    elif bracket['r'] == 0:
-        cnt['l'][bracket['l']] += 1
+    elif l == 0:
+        cnt['r'][r] += 1
+    elif r == 0:
+        cnt['l'][l] += 1
 
 # 集計結果から、いくつペアが作れるか数える
 ans = 0
