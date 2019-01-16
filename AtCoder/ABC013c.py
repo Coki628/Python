560dedcd75ea862cc2b0a2ab0ffea03da9da1c49
MYCOMPUTER\srcn4 <srcn4frdm.c@gmail.com>
2019-01-16 23:19:16 +0700

    変な重複を修正

--- AtCoder/ABC013c.py
+++ AtCoder/ABC013c.py
@@ -31,32 +31,3 @@ def dfs(money, hp, day):
  
 dfs(0, H, 0)
 print(total)
-# -*- coding: utf-8 -*-
-
-import sys
-
-# 再帰呼び出しの回数制限(デフォルト1000)
-sys.setrecursionlimit(10 ** 9)
-
-N, H = map(int, input().split())
-A, B, C, D, E = map(int, input().split())
-
-total = float('inf')
-def dfs(money, hp, day):
-    # 規定の日数に達したら、金額を確認する
-    global total
-    if day == N:
-        total = min(money, total)
-        return
-
-    if hp <= E:
-        # 普通の食事をする
-        dfs(money + A, hp + B, day + 1)
-        # 質素な食事をする
-        dfs(money + C, hp + D, day + 1)
-    else:
-        # 食べない
-        dfs(money, hp - E, day + 1)
-
-dfs(0, H, 0)
-print(total)
