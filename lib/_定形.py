e95a5753a1298b24f7b0f68cdf0dc89f578cd106
MYCOMPUTER\srcn4 <srcn4frdm.c@gmail.com>
2019-04-03 00:24:56 +0900

    ライブラリ更新

--- lib/_定形.py
+++ lib/_定形.py
@@ -404,3 +404,50 @@ class WeightedUnionFind:
     # xからyへのコスト
     def diff(self, x, y):
         return self.weight[x] - self.weight[y]
+
+
+class BipartiteMatching:
+    """
+    XとYの二部グラフの最大マッチング X={0,1,2,...|X|-1} Y={0,1,2,...,|Y|-1}
+    edges[x]: xとつながるYの頂点のset
+    match1[x]: xとマッチングされたYの頂点
+    match2[y]: yとマッチングされたXの頂点
+    """
+
+    def __init__(self, n, m):
+        self.n = n
+        self.m = m
+        self.edges = [set() for _ in range(n)]
+        self.match1 = [-1] * n
+        self.match2 = [-1] * m
+ 
+    def dfs(self, v, visited):
+        """
+        :param v: X側の未マッチングの頂点の1つ
+        :param visited: 空のsetを渡す（外部からの呼び出し時）
+        :return: 増大路が見つかればTrue
+        """
+        for u in self.edges[v]:
+            if u in visited:
+                continue
+            visited.add(u)
+            if self.match2[u] == -1 or self.dfs(self.match2[u], visited):
+                self.match2[u] = v
+                self.match1[v] = u
+                return True
+        return False
+    
+    def add(self, a, b):
+        self.edges[a].add(b)
+
+    def whois1(self, a):
+        """ :param: グループ1の頂点 :return: ペアになるグループ2の頂点 """
+        return self.match1[a]
+
+    def whois2(self, a):
+        """ :param: グループ2の頂点 :return: ペアになるグループ1の頂点 """
+        return self.match2[a]
+
+    def solve(self):
+        # 増大路発見に成功したらTrue(=1)。合計することでマッチング数となる
+        return sum(self.dfs(i, set()) for i in range(self.n))
