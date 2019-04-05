b99918993d7e7786e59b7a5b051191234f4d105a
MYCOMPUTER\srcn4 <srcn4frdm.c@gmail.com>
2019-04-05 10:14:52 +0900

    ちょっと追加

--- AtCoder/chokudai_s001h2.py
+++ AtCoder/chokudai_s001h2.py
@@ -2,11 +2,11 @@
 
 """
 参考：https://ikatakos.com/pot/programming_algorithm/dynamic_programming/longest_common_subsequence
-・LIS(最長増加部分列)の長さ
+・LIS(最長増加部分列)
+・ソート済のAをBとすれば、AとBのLCSに帰着できる。(今回は制約的にダメぽいけど)
 """
 
 import sys
-from bisect import bisect_left
 
 def input(): return sys.stdin.readline().strip()
 def list2d(a, b, c): return [[c] * b for i in range(a)]
@@ -25,17 +25,21 @@ MOD = 10 ** 9 + 7
 
 N=INT()
 A=LIST()
+B=sorted(A)
 
-def LIS(A: list):
-    L = [A[0]]
-    for a in A[1:]:
-        if a > L[-1]:
-            # Lの末尾よりaが大きければ増加部分列を延長できる
-            L.append(a)
-        else:
-            # そうでなければ、「aより小さい最大要素の次」をaにする
-            # 該当位置は、二分探索で特定できる
-            L[bisect_left(L, a)] = a
-    return len(L)
+dp = [[0] * (N+1) for i in range(N+1)]
 
-print(LIS(A))
+for i in range(N+1):
+    for j in range(N+1):
+        # 下に送る
+        if i < N:
+            dp[i+1][j] = max(dp[i+1][j], dp[i][j])
+        # 右に送る
+        if j < N:
+            dp[i][j+1] = max(dp[i][j+1], dp[i][j])
+        # 今回増やす文字が一致するなら、LCSが1文字増える
+        if i < N and j < N and A[i] == B[j]:
+            # 右下に送る
+            dp[i+1][j+1] = max(dp[i+1][j+1], dp[i][j]+1)
+
+print(dp[N][N])
