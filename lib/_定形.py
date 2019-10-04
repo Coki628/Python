bd086007c9efed8cab52f06e761c4d122b39234a
Coki <srcn4frdm.c@gmail.com>
2019-10-04 23:30:22 +0800

    ライブラリ更新

--- lib/_定形.py
+++ lib/_定形.py
@@ -19,6 +19,7 @@ from fractions import Fraction
 from string import ascii_lowercase, ascii_uppercase, digits
 from os.path import commonprefix
 
+# def input(): return sys.stdin.buffer.readline().strip()
 def input(): return sys.stdin.readline().strip()
 def list2d(a, b, c): return [[c] * b for i in range(a)]
 def list3d(a, b, c, d): return [[[d] * c for j in range(b)] for i in range(a)]
@@ -472,7 +473,7 @@ def dijkstra(N: int, nodes: list, src: int) -> list:
     """ ダイクストラ高速化版(頂点数, 隣接リスト(0-indexed), 始点) """
     from heapq import heappush, heappop
 
-    # 頂点[ある始点からの最短距離] (経路自体を知りたい時はここに前の頂点も持たせる)
+    # 頂点[ある始点からの最短距離]
     res = [INF] * N
     # スタート位置
     que = [src]
@@ -495,7 +496,7 @@ def dijkstra(N: int, nodes: list, src: int) -> list:
     return res
 
 def dijkstra(N: int, nodes: list, src: int) -> list:
-    """ ダイクストラ高速化版(頂点数, 隣接リスト(0-indexed), 始点) """
+    """ ダイクストラ高速化版経路付き(頂点数, 隣接リスト(0-indexed), 始点) """
     from heapq import heappush, heappop
 
     # 頂点(ある始点からの最短距離, 前の頂点)
@@ -885,6 +886,86 @@ class SegTree:
                 s = self.func(s, self.tree[r])
             if l & 1:
                 s = self.func(s, self.tree[l])
+                l += 1
+            l >>= 1
+            r >>= 1
+        return s
+
+
+class SegTreeIndex:
+    """
+    以下のクエリを処理する
+    1.update:  i番目の値をxに更新する
+    2.query: 区間[l, r)の値とindex(同値があった場合は一番左)を得る
+    """
+ 
+    def __init__(self, n, func, init):
+        """
+        :param n: 要素数(0-indexed)
+        :param func: 値の操作に使う関数(min, max)
+        :param init: 要素の初期値(単位元)
+        """
+        self.n = n
+        self.func = func
+        self.init = init
+        # nより大きい2の冪数
+        n2 = 1
+        while n2 < n:
+            n2 <<= 1
+        self.n2 = n2
+        self.tree = [self.init] * (n2 << 1)
+        self.index = [self.init] * (n2 << 1)
+        # 1段目(最下段)の初期化
+        for i in range(n2):
+            self.index[i+n2] = i
+        # 2段目以降の初期化
+        for i in range(n2-1, -1, -1):
+            # 全部左の子の値に更新
+            self.index[i] = self.index[i*2]
+
+    def update(self, i, x):
+        """
+        i番目の値をxに更新
+        :param i: index(0-indexed)
+        :param x: update value
+        """
+        i += self.n2
+        self.tree[i] = x
+        while i > 1:
+            left, right = min(i, i^1), max(i, i^1)
+            if self.func(self.tree[left], self.tree[right]) == self.tree[left]:
+                self.tree[i >> 1] = self.tree[left]
+                self.index[i >> 1] = self.index[left]
+            else:
+                self.tree[i >> 1] = self.tree[right]
+                self.index[i >> 1] = self.index[right]
+            i >>= 1
+ 
+    def query(self, a, b):
+        """
+        [a, b)の値を得る
+        :param a: index(0-indexed)
+        :param b: index(0-indexed)
+        """
+        l = a + self.n2
+        r = b + self.n2
+        s = (self.init, -1)
+        while l < r:
+            if r & 1:
+                r -= 1
+                res = self.func(s[0], self.tree[r])
+                # 左との一致を優先する
+                if res == s[0]:
+                    pass
+                else:
+                    s = (self.tree[r], self.index[r])
+            if l & 1:
+                res = self.func(self.tree[l], s[0])
+                # 左との一致を優先する
+                if res == self.tree[l]:
+                    s = (self.tree[l], self.index[l])
+                else:
+                    pass
                 l += 1
             l >>= 1
             r >>= 1
