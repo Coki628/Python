6d7ec96b2ab7a64a280483c894fccac68623a8ed
Coki <srcn4frdm.c@gmail.com>
2019-10-07 13:14:29 +0800

    古いソースを他と合わせた名前に変更

--- AtCoder/ABC001d4.py
+++ AtCoder/ABC001d4.py
@@ -1,71 +1,69 @@
 # -*- coding: utf-8 -*-
-
-"""
-・D自力AC！
-・大昔にやって(きっと)全然できなかったやつ、完全に忘れてるので再挑戦。ACできて嬉しい。
-・いもす法
-・扱うのが時間なので、末端の調整が抜けてて何度かWAしたけど、最終的にちゃんと解決。
-・座標圧縮はめんどいのでやらなかったけど、ループを5毎に飛ばして短くした。
-　→速度変わらず。。
-"""
-
-import sys
-from itertools import accumulate
-
-def input(): return sys.stdin.readline().strip()
-def list2d(a, b, c): return [[c] * b for i in range(a)]
-def list3d(a, b, c, d): return [[[d] * c for j in range(b)] for i in range(a)]
-def ceil(x, y=1): return int(-(-x // y))
-def INT(): return int(input())
-def MAP(): return map(int, input().split())
-def LIST(): return list(map(int, input().split()))
-def Yes(): print('Yes')
-def No(): print('No')
-def YES(): print('YES')
-def NO(): print('NO')
-sys.setrecursionlimit(10 ** 9)
-INF = float('inf')
-MOD = 10 ** 9 + 7
-
-N = INT()
-SE = []
-for i in range(N):
-    s, e = map(int, input().split('-'))
-    SE.append((s, e))
-
-for i in range(N):
-    s, e = SE[i]
-    # 5区切りで下に丸める
-    if s%5 != 0:
-        s -= s%5
-    # 5区切りで上に丸める
-    if e%5 != 0:
-        e += 5 - e%5
-        # 時間の繰り上がりを調整
-        if str(e)[-2:] == '60':
-            e += 40
-    SE[i] = (s, e)
-
-# imosで区間をまとめる
-imos = [0] * 2402
-for s, e in SE:
-    imos[s] += 1
-    imos[e+1] -= 1
-imos = list(accumulate(imos))
-
-is_raining = False
-ans = []
-# 5毎で確認していく
-for i in range(0, 2402, 5):
-    if is_raining:
-        if imos[i+1] == 0:
-            is_raining = False
-            e = i
-            ans.append((s, e))
-    else:
-        if imos[i] > 0:
-            is_raining = True
-            s = i
-
-for s, e in ans:
-    print('{0}-{1}'.format(format(s, '04'), format(e, '04')))
+ 
+# 切り下げるメソッド
+def roundDownTime(time_str):
+    # 0～4は0に
+    if 0 <= int(time_str[3:4]) <= 4:
+        return time_str[0:3] + "0"
+    # 5～9は5に
+    if 5 <= int(time_str[3:4]) <= 9:
+        return time_str[0:3] + "5"
+ 
+# 切り上げるメソッド
+def roundUpTime(time_str):
+    # 0はそのまま
+    if int(time_str[3:4]) == 0:
+        return time_str
+    # 1～5は5に
+    if 1 <= int(time_str[3:4]) <= 5:
+        return time_str[0:3] + "5"
+    # 6～9は0に
+    if 6 <= int(time_str[3:4]) <= 9:
+        # 50分台だけは時のケタが動く
+        if time_str[2:3] == "5":
+            return str(int(time_str[0:2]) + 1) + "00"
+        # 通常は10分のケタが動く
+        else:
+            return time_str[0:2] + str(int(time_str[2:3]) + 1) + "0"
+ 
+# 入力の数値
+counter = int(input())
+input_list = [input() for _ in range(counter)]
+ 
+# 開始時刻と終了時刻のペアで二次元リストを作る
+time_list = [[0 for col in range(2)] for row in range(counter)]
+i = 0
+for input_str in input_list:
+    # 丸め処理を完了させる
+    time_list[i][0] = roundDownTime(input_str[0:4])
+    time_list[i][1] = roundUpTime(input_str[5:9])
+    i += 1
+ 
+# 5分毎の時刻を格納する辞書
+time_dict = {}
+for i in range(0, 2401, 5):
+    time_dict[i] = False
+ 
+# 開始終了ペアの数でループ
+for i in range(counter):
+    start_time = int(time_list[i][0])
+    end_time = int(time_list[i][1])
+    # 開始時刻から終了時刻までの辞書の値をTrueにする
+    for j in range(start_time, end_time, 5):
+        time_dict[j] = True
+ 
+# 辞書をキーでソート
+sorted_dict = sorted(time_dict.items())
+ 
+# フラグを使って開始終了の境界を判別する
+isRain = False
+for key, value in sorted_dict:
+    if value is True and isRain is False:
+        isRain = True
+        # 桁埋めして文字列に戻す
+        start_time = str("%04d" % key)
+    if value is False and isRain is True:
+        isRain = False
+        end_time = str("%04d" % key)
+        # 条件に当てはまった開始終了ペアを出力
+        print(start_time + "-" + end_time)
